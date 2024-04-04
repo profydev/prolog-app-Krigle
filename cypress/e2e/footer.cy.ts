c999502662f9aecf01436ed1bdef73d8bbf1035d
Krigle <130289154+Krigle@users.noreply.github.com>
2024-04-04 12:34:07 +0100

    Typo in footer test

--- cypress/e2e/footer.cy.ts
+++ cypress/e2e/footer.cy.ts
@@ -2,7 +2,7 @@ import { version } from "../../package.json";
 
 describe("Footer", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/dashboard/dashboard");
+    cy.visit("http://localhost:3000/dashboard");
   });
 
   it("Should show the version number from package.json", () => {
