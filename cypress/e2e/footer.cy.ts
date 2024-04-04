12cae8e9071ffed26b70676ac5a97c3bf5fbbc7a
Krigle <130289154+Krigle@users.noreply.github.com>
2024-04-04 11:28:28 +0100

    Tested footer

---
+++ cypress/e2e/footer.cy.ts
@@ -0,0 +1,11 @@
+import { version } from "../../package.json";
+
+describe("Footer", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/dashboard/dashboard");
+  });
+
+  it("Should show the version number from package.json", () => {
+    cy.get(".footer").contains(`Version: ${version}`);
+  });
+});
